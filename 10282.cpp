da53471a451bb23ac5234e933d195e3b31be3b67
Ansarul14 <ansarul.islam117@gmail.com>
2017-08-04 02:36:32 +0600

    Add files via upload

---
+++ 10282.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1,s2,s3;
+   // getchar();
+   map<string,string>mp;
+   map<string,string>::iterator it;
+    getline(cin,s1);
+    while(s1!="")
+    {
+         stringstream ss(s1);
+         ss>>s2>>s3;
+         mp[s3]=s2;
+        getline(cin,s1);
+    }
+    while(cin>>s1)
+    {
+        it=mp.find(s1);
+        if (it==mp.end())
+        {
+            cout<<"ch"<<endl;
+        }
+        else
+        {
+            cout<<it->second<<endl;
+        }
+    }
+}
